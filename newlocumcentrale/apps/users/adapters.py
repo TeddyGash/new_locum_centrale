8336748e9c25361266d1a0e57a2ce6d6561eb614
Theodore <30gash@gmail.com>
2023-07-01 10:45:04 +0000

    save retrieved data using request.session via adapters.py

--- newlocumcentrale/apps/users/adapters.py
+++ newlocumcentrale/apps/users/adapters.py
@@ -10,6 +10,17 @@ class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
 
+    def save_user(self, request, user, form, commit=True):
+        response_data = request.session.get("response_data")
+        if response_data:
+            user.verification_status = response_data.get("verification_status")
+            user.contact = response_data.get("phone")
+            user.register_type = response_data.get("register_type")
+            user.date_of_provisional_reg = response_data.get("year_of_provisional")
+            user.category = response_data.get("category")
+
+        return super().save_user(request, user, form, commit)
+
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest, sociallogin: Any):
